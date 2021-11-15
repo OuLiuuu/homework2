6d1bef890fdc4f74e86b0c0c9208ce0f5b73de04
Liu Ou <1293688015@qq.com>
2021-11-15 23:17:02 +0800

    step 2

--- hw2/albumcollection.py
+++ hw2/albumcollection.py
@@ -10,3 +10,40 @@ o save albums (from album list into csv file)
 o add album – add a single Album object to the albums attribute
 o sort (by the key passed in, then by year) (attrgetter from [2])
 """
+
+import csv
+from album import Album
+
+
+class AlbumCollection:
+    def __init__(self):
+        """
+        constructor
+        """
+        self.albums = []
+
+    def __str__(self):
+        """
+        string to be printed
+        """
+        result = ''
+        for album in self.albums:
+            result += str(album) + '\n'
+        return result
+
+    def load_albums(self, filename):
+        """
+        load albums (from csv file into Album objects in the list)
+        :param filename: file name
+        """
+        with open(filename, "r") as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                title = row[0]
+                artist = row[1]
+                year = int(row[2])
+                is_completed = row[3]
+                if (is_completed == 'r'):
+                    self.albums.append(Album(title, artist, year, False))
+                else:
+                    self.albums.append(Album(title, artist, year, True))
